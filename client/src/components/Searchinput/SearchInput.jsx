960cbad45c556c3ab80862c91365bd4169360cbb
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-06-14 19:47:10 +0200

    Frontend  - fix: search for contacts

--- client/src/components/Searchinput/SearchInput.jsx
+++ client/src/components/Searchinput/SearchInput.jsx
@@ -1,57 +1,45 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { FetchUserContext } from "@/Context/UserContext";
-const fetchContacts = import.meta.env.VITE_API_CONTACTS;
 
 export const SearchInput = () => {
-  const [searchContact, setSearchContact] = useState("");
+  const [searchContact, setSearchContact] = useState(null);
+  const { contacts } = useContext(FetchUserContext);
 
-  const { contactData } = useContext(FetchUserContext);
-
-  const findContact = async (values) => {
-    setSearchContact(values);
-
-    const filterData = await contactData(searchContact);
-    console.log("filterData", filterData);
-
-    if (findContact) {
-      return (
-        <div
-          key={filterData.email}
-          className="flex justify-between items-center p-1"
-        >
-          <div className=" w-full flex items-center justify-between">
-            <img
-              src={filterData.avatar}
-              alt={filterData.name}
-              className="w-8 h-8 rounded-full"
-            />
-            <div className="w-full px-1">
-              <div className="flex justify-between items-center">
-                <div className="flex justify-between w-full">
-                  {filterData.name}
-                  {isAuthStatus ? (
-                    <p className="text-green-400">on</p>
-                  ) : (
-                    <p className="text-red-400">off</p>
-                  )}
-                </div>
-              </div>
-              <p className="text-xs text-gray-500">{filterData.email}</p>
-            </div>
-          </div>
-        </div>
-      );
-    }
-  };
-
+  const filterData = searchContact
+    ? contacts?.filter((contact) =>
+        contact.name.toLowerCase().includes(searchContact.toLowerCase())
+      )
+    : [];
   return (
-    <>
+    <div className="relative w-full">
       <Input
-        onChange={(e) => findContact(e.target.value)}
+        onChange={(e) => setSearchContact(e.target.value)}
         placeholder="Search for Contacts"
         className="border-b-0 rounded-b-xs"
       />
-    </>
+      {filterData.length > 0 && (
+        <div className="absolute w-full bg-gray-300 z-5 p-1">
+          {filterData?.map((contact) => (
+            <div
+              key={contact.email}
+              className="flex justify-between rounded-lg p-1 cursor-pointer transition duration-300 hover:bg-gray-400"
+            >
+              <div className=" w-full flex items-center justify-between ">
+                <img
+                  src={contact.avatar}
+                  alt={contact.name}
+                  className="w-8 h-8 rounded-full"
+                />
+                <div className="w-full px-1">
+                  <p className="text-xs text-balck-500">{contact.name}</p>
+                  <p className="text-xs text-gray-500">{contact.email}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
