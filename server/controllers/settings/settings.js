9456001207e35d521c242492bb977ac3544ae5ab
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-06-23 21:36:04 +0200

    Backend  - replace validation check from middleware (buffer issues => undefined) to the controller

--- server/controllers/settings/settings.js
+++ server/controllers/settings/settings.js
@@ -1,19 +1,51 @@
 const User = require("../../models/userSchema");
 const path = require("path");
 const fs = require("fs");
+const { fileTypeFromBuffer } = require("file-type");
 
 exports.saveImage = async (req, res) => {
   const user = req.user;
-  const uploadedImgPath = req.file;
-  const newAvatarImg = uploadedImgPath.path.replace(/\\/g, "/");
+  const dataBuffer = req.file.buffer; // Der Dateiinhalt
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+
+  const result = await fileTypeFromBuffer(req.file.buffer);
+
+  if (
+    !result ||
+    !["image/jpeg", "image/png", "image/svg+xml"].includes(result.mime)
+  ) {
+    return res.status(400).json({ error: "Invalid file type" });
+  }
+  const fileExt = path.extname(req.file.originalname);
+  const uniqueName =
+    Date.now() +
+    "-" +
+    user._id +
+    "-" +
+    Math.round(Math.random() * 1e9) +
+    fileExt;
+  const finalPath = path.join(
+    __dirname,
+    "..",
+    "..",
+    "uploads",
+    "avatars",
+    uniqueName
+  );
+
   try {
+    fs.writeFileSync(finalPath, dataBuffer);
+    const avatarPathForDB = "uploads/avatars/" + uniqueName;
+    const newAvatarImg = avatarPathForDB.replace(/\\/g, "/");
     if (user.avatar && user.avatar.startsWith("uploads/")) {
       const oldImagePath = path.join(__dirname, "..", "..", user.avatar);
       fs.unlink(oldImagePath, (err) => {
         err ? console.log(err) : console.log("Old Img Deleted", user.avatar);
       });
     }
-    const changeImg = await User.findOneAndUpdate(
+    await User.findOneAndUpdate(
       {
         _id: user._id,
       },
--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,16 +1,6 @@
 const multer = require("multer");
-const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/avatars"); // hier werden die Bilder gespeichert
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix =
-      Date.now() + "-" + req.user._id + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
