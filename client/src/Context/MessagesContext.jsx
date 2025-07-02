8278499a8bc58eb3ccb01f4d5396e3bd40d3886e
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-07-02 21:40:38 +0200

    Frontend  - move active chat to top of list on new message

--- client/src/Context/MessagesContext.jsx
+++ client/src/Context/MessagesContext.jsx
@@ -16,6 +16,7 @@ export const ChatDataFlowProvider = ({ children }) => {
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [currentChatMessages, setCurrentChatMessages] = useState([]);
   const [userGotNewMessage, setUserGotNewMessage] = useState({});
+  const [lastMessage, setLastMessage] = useState({});
 
   const fetchChatData = async () => {
     if (!selectedUserId) return;
@@ -61,9 +62,11 @@ export const ChatDataFlowProvider = ({ children }) => {
     (message) => {
       const isActive =
         message.from === selectedUserId || message.to === selectedUserId;
+
       if (isActive) {
         setCurrentChatMessages((prev) => [...prev, message]);
       }
+
       setUserGotNewMessage((prev) => ({
         ...prev,
         [message.from]: (prev[message.from] || 0) + 1,
@@ -75,18 +78,29 @@ export const ChatDataFlowProvider = ({ children }) => {
     [selectedUserId]
   );
 
+  const getLastMessageTimestamp = useCallback((message) => {
+    const latestSender =
+      message.to === selectedUserId ? message.from : message.to;
+    setLastMessage((prev) => ({
+      ...prev,
+      [latestSender]: timeStamp,
+    }));
+  }, []);
+
+  console.log(lastMessage);
+
   useEffect(() => {
     if (!socket) return;
 
     fetchChatData();
-    socket.on("receive_message", handleMessage);
+    socket.on("receive_message", handleMessage, getLastMessageTimestamp);
     socket.on("unread_count_reset", resetUnread);
 
     return () => {
-      socket.off("receive_message", handleMessage);
+      socket.off("receive_message", handleMessage, getLastMessageTimestamp);
       socket.off("unread_count_reset", resetUnread);
     };
-  }, [socket, handleMessage, resetUnread]);
+  }, [socket, handleMessage, getLastMessageTimestamp, resetUnread]);
 
   useEffect(() => {
     if (!socket) return;
